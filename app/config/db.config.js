c3b9d3379e2570cdba9538df08d981ca34aaa214
Edwardpauldrgst <dreagaster@gmail.com>
2022-01-20 09:14:45 +0100

    asdasdbuzivagyok

--- app/config/db.config.js
+++ app/config/db.config.js
@@ -1,8 +1,8 @@
 module.exports = {
   HOST: "localhost",
   USER: "root",
-  PASSWORD: "123456",
-  DB: "testdb",
+  PASSWORD: "",
+  DB: "sudoku_ranglista",
   dialect: "mysql",
   pool: {
     max: 5,
